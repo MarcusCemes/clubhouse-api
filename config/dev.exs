2c6234cbf03c69f89a927079c95db497d5be7f62
Marcus Cemes <marcus.cemes@gmail.com>
2022-06-28 21:37:56 +0200

    chore: configure project

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :clubhouse, Clubhouse.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "clubhouse_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
@@ -17,9 +17,8 @@ config :clubhouse, Clubhouse.Repo,
 # watchers to your application. For example, we use it
 # with esbuild to bundle .js and .css sources.
 config :clubhouse, ClubhouseWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  url: [host: "clubhouse.test", port: 443, scheme: "https"],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
