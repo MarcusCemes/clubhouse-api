e7c1ff219331c577ab2b98c402285bfda2bd4fed
Marcus Cemes <marcus.cemes@gmail.com>
2022-07-25 23:44:15 +0200

    fix: error rendering when :message is not set

--- lib/clubhouse_web/views/error_view.ex
+++ lib/clubhouse_web/views/error_view.ex
@@ -5,4 +5,8 @@ defmodule ClubhouseWeb.ErrorView do
     status_text = Phoenix.Controller.status_message_from_template(template)
     render("error.html", Map.put(assigns, :status_text, status_text))
   end
+
+  defp extract_message(%{reason: %{message: message}}), do: message
+
+  defp extract_message(_), do: nil
 end
